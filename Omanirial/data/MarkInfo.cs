813a37de3df8ba73954964de642d6b62eac4c979
Hidetaka Sasai <sasai@ds-solution.co.jp>
2018-06-27 10:25:39 +0900

    Modify mark info.

--- Omanirial/data/MarkInfo.cs
+++ Omanirial/data/MarkInfo.cs
@@ -1,10 +1,14 @@
 ﻿using System.Drawing;
+using System.Runtime.Serialization;
 
 namespace Omanirial.data
 {
+    [DataContract]
     public class MarkInfo
     {
+        [DataMember]
         public Point Location { get; set; }
+        [DataMember]
         public bool Disabled { get; set; }
         public byte[] Hist { get; set; }
         public int Score { get; set; }
